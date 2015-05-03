3af2f1a03c38ee8fffd65bf2b1280ddf17a9d948
Tim Ermilov <yamalight@gmail.com>
2015-05-03 11:16:52 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Awesome Turris example component
+
+This is an example component scaffolded using Turris.js generator
+
+Made with [Turris.js](https://github.com/turrisjs)
---
+++ src/template.jsx
@@ -0,0 +1,12 @@
+// react
+import React from 'react';
+
+const render = function() {
+    return (
+        <div className="turrisExample-component">
+          I am standalone Turris example component
+        </div>
+    );
+};
+
+export default render;
